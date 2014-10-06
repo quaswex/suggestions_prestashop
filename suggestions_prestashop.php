e363b3d380a5d14f4aaf075e95ce6cef200835e3
Maxim Bokov <maxim.bokov@gmail.com>
2014-10-06 16:08:40 +0400

    Test account creation

--- suggestions_prestashop.php
+++ suggestions_prestashop.php
@@ -34,6 +34,7 @@ class suggestions_prestashop extends Module
 
         if (!parent::install() ||
             !$this->registerHook('displayHeader') ||                               // Добавляем в заголовок CSS и JS
+            !$this->registerHook('createAccountTop') ||
             !Configuration::updateValue('DADATA_SUGGESTIONS_TOKEN','') ||
             !Configuration::updateValue('DADATA_SUGGESTIONS_COUNT',10) ||
             !Configuration::updateValue('DADATA_SUGGESTIONS_HIDE',true) ||
@@ -322,4 +323,8 @@ class suggestions_prestashop extends Module
         return $this->wrapScriptOnLoad();
 
     }
+
+    public function hookCreateAccountTop() {
+    return "<!-- CREATEACOOUNTTOP -->";
+}
 }
